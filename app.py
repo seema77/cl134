cc39b4493fba00b060c27d5e1570c6b5d035a11a
seema77 <76130253+seema77@users.noreply.github.com>
2023-11-10 03:18:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, url_for, request, jsonify
+from text_sentiment_prediction import *
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    return render_template('index.html')
+ 
+@app.route('/predict-emotion',methods=["POST"])
+def predict_emotion():
+    input_text=request.json.get("text")
+
+    if not input_text:
+        response={
+            "status":"error",
+            "message":"Please enter some text to predict emotion !"
+        }
+        return jsonify(response)
+    else:
+        predicted_emotion,predicted_emotion_img_url=predict(input_text)
+
+        response={
+            "status":"success",
+            "data":{
+                "predicted_emotion":predicted_emotion,
+                "predicted_emotion_img_url":predicted_emotion
+            }
+        }
+        return jsonify(response)
+
+       
+app.run(debug=True)
+
+
+
+    
